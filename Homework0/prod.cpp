cc808330f8b4cb3d97bf76bf521460dfcb0b656d
Jamison Orton <jorton4@byu.edu>
2024-09-11 15:13:35 -0600

    Added a small multiply function

--- Homework0/prod.cpp
+++ Homework0/prod.cpp
@@ -1,1 +1,14 @@
 //  :)
+#include <iostream>
+using std::cout, std::endl, std::cin;
+
+int main() {
+    int a;
+    int b;
+
+    cout << "Enter a single number: ";
+    cin >> a;
+    cout << "Enter another number: ";
+    cin >> b;
+    cout << "The sum of " << a << " * " << b << " is " << a * b << endl;
+}
