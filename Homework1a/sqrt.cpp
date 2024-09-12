28ee8eb71a69b2e2483a8f96862bad7ca1ee58cf
Jamison Orton <jorton4@byu.edu>
2024-09-12 14:45:27 -0600

    Started on the sqrt

--- Homework1a/sqrt.cpp
+++ Homework1a/sqrt.cpp
@@ -1,6 +1,19 @@
 #include <cstdlib>  // atof
+#include <iostream>
 using std::abs;  
+using std::cout;
+using std::endl;
 
 const double EPSILON = 0.0001;
 
-// :)
+int main(int argc, char* argv[]) {
+    int start = 0;
+    double val = atof(argv[1]);
+
+    //upper bound
+    int firstUpper = val * val;
+    if (firstUpper > val) {
+        int secondUpper = (val - 1) * (val - 1);
+        cout << secondUpper << endl;
+    }
+}
