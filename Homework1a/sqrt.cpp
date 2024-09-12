355d8571bd817e43d7539f55a44cec18b0a903cf
Jamison Orton <jorton4@byu.edu>
2024-09-12 15:44:27 -0600

    changes for the while loop

--- Homework1a/sqrt.cpp
+++ Homework1a/sqrt.cpp
@@ -9,11 +9,14 @@ const double EPSILON = 0.0001;
 int main(int argc, char* argv[]) {
     int start = 0;
     double val = atof(argv[1]);
+    int upper = val * val;
+    int lower;
 
-    //upper bound
-    int firstUpper = val * val;
-    if (firstUpper > val) {
-        int secondUpper = (val - 1) * (val - 1);
-        cout << secondUpper << endl;
+    while (upper > val) {
+        if ()
+        {
+            /* code */
+        }
+        
     }
 }
