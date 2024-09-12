10bd3c61e8c7bd376321a02b3713db5c864fc81b
Jamison Orton <jorton4@byu.edu>
2024-09-11 23:05:29 -0600

    work on getting the starting number being divisible

--- Homework1a/numbers_divisible_by.cpp
+++ Homework1a/numbers_divisible_by.cpp
@@ -8,33 +8,15 @@ int main(int argc, char* argv[]) {
     int divisor = atoi(argv[3]);
 
     if (argv[1] < argv[2]) {
-        for (int i=start/divisor;i<=end;i=i+divisor){
+        //work on the first starting number being divisible by the divisor
+        for (int i=start*divisor;i<=end;i=i+divisor){
             cout << i << endl;
         }
     }
-    
     // else if (argv[1] > argv[2]) {
     //     for (int i=start;i>=end;i=i-divisor) {
     //         cout << i << endl;
     //     }
     // }
-    // cout << start << endl << start/2 << endl << end << endl;
-    // return 0;
-    // Condition to check for 3 numbers and print the error
-    // if (argc < 4) {
-    //     printf("divisible requires 3 arguments\n");
-    //     // cerr << "prod requires 3 arguments" << endl;
-    //     return 1;
-    // }
-    // else if (argv[1] < argv[2]) {
-    //     cout << "Find the divisors" << endl;
-    // }
-    // // making floats work for decimal numbers
-    // int a = atof(argv[1]);
-    // int b = atof(argv[2]);
-    // int c = atof(argv[3]);
-    // int divisible = a / b;
-    // // Final display
-    // cout << divisible << endl;
-    // return 0;
+    return 0;
 }
