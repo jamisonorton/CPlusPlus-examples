3085449d551783163cfba22079f86dc496aaef26
Jamison Orton <jorton4@byu.edu>
2024-09-12 10:38:12 -0600

    collatz done

--- Homework1a/collatz.cpp
+++ Homework1a/collatz.cpp
@@ -1,1 +1,20 @@
-// :)
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[]) {
+    int start = atoi(argv[1]);
+    cout << start << endl;
+
+    while (start > 1) {
+        if (start % 2 == 0) {
+            start /= 2;
+            cout << start << endl;
+        }
+        else {
+            start = 3 * start + 1;
+            cout << start << endl;
+        }
+    }
+    return 0;
+}
