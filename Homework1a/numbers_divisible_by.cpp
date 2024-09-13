5722f82bc3e3af5a59fcfd77e2ca1a8cb94c0a43
Jamison Orton <jorton4@byu.edu>
2024-09-13 15:43:47 -0600

    finished Final

--- Homework1a/numbers_divisible_by.cpp
+++ Homework1a/numbers_divisible_by.cpp
@@ -6,43 +6,43 @@ int main(int argc, char* argv[]) {
     int start = atoi(argv[1]);
     int end = atoi(argv[2]);
     int divisor = atoi(argv[3]);
-    
-/*  If you subtract from start the modulo of the divisor then add the divisor 
-    for incrementing up it will give you the correct starting value and now
-    you only have to modulo for the first given number 
-    i.e going 3 to 16 with divisor 2 We start at 3 when we subtract (3%2) you get 2
-    then add the divisor (2) which starts you at 4 which is divisible by 
-    the divisor */
-    if (start < end) {
-        start = start - (start % divisor) + divisor;
-        for (int i = start; i <= end; i += divisor)
-        cout << i << endl;
-    }
-/*  If you subtract from start the modulo of the divisor for incrementing 
-    down it will give you the correct starting value and now
-    you only have to modulo for the first given number
-    i.e going 15 to 3 with divisor 2 We start at 15 and subtract 15%2 (1) 
-    starting you at 14 which is divisible by the divisor */
-    else {
-        start = start - (start % divisor);
-        for (int i = start; i >= end; i -= divisor)
-        cout << i << endl;
-    }
 
-//old way that works but is expensive
-    // if (start < end) {
-    //     for (int i = start; i <= end; i+=divisor){
-    //         if (i % divisor == 0) {
-    //             cout << i << endl;
-    //         }
-    //     }
-    // }
-    // else {
-    //     for (int i = start; i >= end; i--) {
-    //         if (i % divisor == 0) {
-    //             cout << i << endl;
-    //         }
-    //     }
-    // }
+//why does it work when I use start and end but not argv[1] and argv[2]
+    if (start < end) {
+        for (int i = start; i <= end; i++){
+            if (i % divisor == 0) {
+                cout << i << endl;
+            }
+        }
+    }
+    else {
+        for (int i = start; i >= end; i--) {
+            if (i % divisor == 0) {
+                cout << i << endl;
+            }
+        }
+    }
     return 0;
 }
+
+// /*  If you subtract from start the modulo of the divisor then add the divisor 
+//     for incrementing up it will give you the correct starting value and now
+//     you only have to modulo for the first given number 
+//     i.e going 3 to 16 with divisor 2 We start at 3 when we subtract (3%2) you get 2
+//     then add the divisor (2) which starts you at 4 which is divisible by 
+//     the divisor */
+//     if (start < end) {
+//         start = start - (start % divisor) + divisor;
+//         for (int i = start; i <= end; i += divisor)
+//         cout << i << endl;
+//     }
+// /*  If you subtract from start the modulo of the divisor for incrementing 
+//     down it will give you the correct starting value and now
+//     you only have to modulo for the first given number
+//     i.e going 15 to 3 with divisor 2 We start at 15 and subtract 15%2 (1) 
+//     starting you at 14 which is divisible by the divisor */
+//     else {
+//         start = start - (start % divisor);
+//         for (int i = start; i >= end; i -= divisor)
+//         cout << i << endl;
+//     }
--- Homework1a/sqrt.cpp
+++ Homework1a/sqrt.cpp
@@ -7,16 +7,27 @@ using std::endl;
 const double EPSILON = 0.0001;
 
 int main(int argc, char* argv[]) {
-    int start = 0;
-    double val = atof(argv[1]);
-    int upper = val * val;
-    int lower;
-
-    while (upper > val) {
-        if ()
-        {
-            /* code */
-        }
-        
+    double n = atof(argv[1]);
+    double high = n;
+    double low = 0;
+    double mid = low + (high - low) / 2;
+    // cout << mid << " (" << mid * mid << ")" << endl;
+    
+    if (high < 0) {
+        printf("Input must be greater than 0\n");
+        return 1;
     }
+    else {
+        while (abs((mid * mid) - n) > EPSILON) {
+            mid = low + (high - low) / 2;
+            if (mid * mid > n) {
+                high = mid;
+            }
+            else if (mid * mid < n) {
+                low = mid;
+            }
+            cout << mid << " (" << mid * mid << ")" << endl;
+        }
+    }
+    return 0;
 }
