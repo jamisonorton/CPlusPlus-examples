04b404745b0294b57af95f08e38ae7af1c3f506e
Jamison Orton <jorton4@byu.edu>
2024-09-17 16:53:34 -0600

    finished calculator?

--- Homework1b/calculator.cpp
+++ Homework1b/calculator.cpp
@@ -17,69 +17,79 @@ int main() {
     string divide = "divide";
     string mod = "mod";
 
-    // int num1 = stoi(left);
-    // int num2 = stoi(right);
-
     cout << "operation: ";
     getline(cin, op);
 
-    if (op != "add" && op != "subtract" && op != "multiply" && op != "divide" && op != "mod") {
-        cout << op << " isn't a valid operation" << endl;
-    }
+    while (op != "") {
+        if (op != "add" && op != "subtract" && op != "multiply" && op != "divide" && op != "mod") {
+            cout << op << " isn't a valid operation" << endl;
+            cout << "operation: ";
+            getline(cin, op);
+        }
 
-    if (op == "add") {
-        cout << "left operand: ";
-        getline(cin, left);
-        cout << "right operand: ";
-        getline(cin, right);
-        int num1 = stoi(left);
-        int num2 = stoi(right);
-        cout << num1 + num2 << endl;
-    }
+        if (op == "add") {
+            cout << "left operand: ";
+            getline(cin, left);
+            cout << "right operand: ";
+            getline(cin, right);
+            int num1 = stoi(left);
+            int num2 = stoi(right);
+            cout << num1 + num2 << endl;
+            cout << "operation: ";
+            getline(cin, op);
+        }
 
-    if (op == "subtract") {
-        cout << "left operand: ";
-        getline(cin, left);
-        cout << "right operand: ";
-        getline(cin, right);
-        int num1 = stoi(left);
-        int num2 = stoi(right);
-        cout << num1 - num2 << endl;
-    }
+        if (op == "subtract") {
+            cout << "left operand: ";
+            getline(cin, left);
+            cout << "right operand: ";
+            getline(cin, right);
+            int num1 = stoi(left);
+            int num2 = stoi(right);
+            cout << num1 - num2 << endl;
+            cout << "operation: ";
+            getline(cin, op);
+        }
 
-    if (op == "multiply") {
-        cout << "left operand: ";
-        getline(cin, left);
-        cout << "right operand: ";
-        getline(cin, right);
-        int num1 = stoi(left);
-        int num2 = stoi(right);
-        cout << num1 * num2 << endl;
-    }
+        if (op == "multiply") {
+            cout << "left operand: ";
+            getline(cin, left);
+            cout << "right operand: ";
+            getline(cin, right);
+            int num1 = stoi(left);
+            int num2 = stoi(right);
+            cout << num1 * num2 << endl;
+            cout << "operation: ";
+            getline(cin, op);
+        }
 
-    if (op == "divide") {
-        cout << "left operand: ";
-        getline(cin, left);
-        cout << "right operand: ";
-        getline(cin, right);
-        int num1 = stoi(left);
-        int num2 = stoi(right);
-        cout << num1 / num2 << endl;
-    }
+        if (op == "divide") {
+            cout << "left operand: ";
+            getline(cin, left);
+            cout << "right operand: ";
+            getline(cin, right);
+            int num1 = stoi(left);
+            int num2 = stoi(right);
+            cout << num1 / num2 << endl;
+            cout << "operation: ";
+            getline(cin, op);
+        }
 
-    if (op == "mod") {
-        cout << "left operand: ";
-        getline(cin, left);
-        cout << "right operand: ";
-        getline(cin, right);
-        int num1 = stoi(left);
-        int num2 = stoi(right);
-        cout << num1 % num2 << endl;
+        if (op == "mod") {
+            cout << "left operand: ";
+            getline(cin, left);
+            cout << "right operand: ";
+            getline(cin, right);
+            int num1 = stoi(left);
+            int num2 = stoi(right);
+            cout << num1 % num2 << endl;
+            cout << "operation: ";
+            getline(cin, op);
+        }
     }
 
     if (op == "") {
-        return 1;
+        return 0;
     }
-
     return 0;
 }
