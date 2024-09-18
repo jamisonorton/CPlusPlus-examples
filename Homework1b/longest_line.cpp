ae4918d0271f38aa8afe320f8ca918f33ce55a69
Jamison Orton <jorton4@byu.edu>
2024-09-17 23:54:38 -0600

    can print out the number of words on a line. Figure out how to return the line with the most number of words

--- Homework1b/longest_line.cpp
+++ Homework1b/longest_line.cpp
@@ -1,1 +1,61 @@
-//  :-)
+#include <iostream>
+using std::cout, std::cin, std::cerr, std::getline, std::endl, std::stoi;
+
+#include <string>
+using std::string;
+
+#include <fstream>
+using std::ifstream, std::ofstream;
+
+#include <sstream>
+using std::stringstream;
+
+// int countWords (string word) {
+//     stringstream word_stream(word);
+//     int total = 0;
+//     int num;
+//     while (word_stream >> num) {
+//         total += num;
+//     }
+//     return total;
+// }
+
+// int main (int argc, char *argv[]) {
+
+//     if (argc < 2) {
+//         cerr << "you need both an input and output file" << endl;
+//         exit(1);
+//         }
+
+//     ifstream infile(argv[1]);
+//     if (!infile.is_open()) {
+//         cerr << "unable to open file" << argv[1] << endl;
+//         exit(2);
+//     }    
+
+//     string line;
+//     while (getline(infile, line)) {
+//         cout << countWords(line) << endl;
+//     }
+
+//     return 0;
+
+//     infile.close();
+// }
+
+int main(int argc, char *argv[]) {
+    string line;
+    ifstream infile(argv[1]);
+    while (getline(infile, line)) {
+        stringstream stream(line);
+        string word;
+        int wordCount = 0;
+
+        while (stream >> word) {
+            ++wordCount;
+        }   
+    cout << wordCount << endl;
+    }
+
+    return 0;
+}
--- Homework1b/reformat.cpp
+++ Homework1b/reformat.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using std::cout, std::cin, std::getline, std::endl, std::stod;
+using std::cout, std::cin, std::cerr, std::getline, std::endl, std::stod;
 
 #include <string>
 using std::string;
@@ -8,7 +8,35 @@ using std::string;
 using std::ifstream, std::ofstream;
 
 int main (int argc, char *argv[]) {
-    if (argc < 4) {
+    string fname = "";
+    string lname = "";
+    int points;
+    double factor;
+    
+    //first arg is input file, 2nd arg is output
+    if (argc < 3) {
+        cerr << "you need both an input and output file" << endl;
+        exit(1);
+        }
 
+    ifstream infile(argv[1]);
+    if (!infile.is_open()) {
+        cerr << "unable to open file" << argv[1] << endl;
+        exit(2);
     }
+
+    ofstream outfile(argv[2]);
+    if (!outfile.is_open()) {
+        cerr << "unable to open file" << argv[2] << endl;
+        exit(3);
+    }
+// read the infile and output it with the correct sequence. Also while loop makes sure that it does every line 1 time until the end of the file
+    string line;
+    while (infile >> fname >> lname >> points >> factor) {
+        outfile << lname << ", " << fname << ": " << points * factor << endl;
+    }
+    return 0;
+
+    infile.close();
+    outfile.close();
 }
