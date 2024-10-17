f855ad4d33e68287bb5af1a2845e653d11facb2c
Jamison Orton <jorton4@byu.edu>
2024-10-17 08:09:29 -0600

    added the set

--- Homework2b/VectorSet.h
+++ Homework2b/VectorSet.h
@@ -1,42 +1,49 @@
 #pragma once
 
+#include <vector>
+
 template<class T>
 class VectorSet {
+private:
+    std::vector<T> elements;
+
 public:
     bool contains(T item) const {
-        // implement contains here
-
-        // return true if item is in the set and false if not
+        for (const auto& elem : elements) {
+            if (elem == item) {
+                return true;
+            }
+        }
+        return false;
     }
 
     bool insert(T item) {
-        // implement insert here
-
-        // return true if item is inserted and false if item is already in the
-        // set
+        if (!contains(item)) {
+            elements.push_back(item);
+            return true;
+        }
+        return false;
     }
 
     bool remove(T item) {
-        // implement remove here
-
-        // return true if item is removed and false if item wasn't in the set
+        for (auto it = element.begin(); it != elements.end(); ++it) {
+            if (*it == item) {
+                elements.erase(it);
+                return true;
+            }
+        }
+        return false;
     }
 
     int size() const {
-        // implement size here
-
-        // return the number of items in the set
+        return elements.size();
     }
 
     bool empty() const {
-        // implement empty here
-
-        // return true if the set is empty and return false otherwise
+        return elements.empty();
     }
 
     void clear() {
-        // implement clear here
-
-        // remove all items from the set
+        elements.clear();
     }
 };
