5c709c2ff85ef88cb38f330ac1e9441bb48661f4
Jamison Orton <jorton4@byu.edu>
2024-12-10 08:22:21 -0700

    Finished DLL

--- Homework4b/DLList.h
+++ Homework4b/DLList.h
@@ -1,18 +1,12 @@
 #pragma once
 
 #include <iostream>
-#include <string>
+#include <stdexcept>
 
 template<class T>
 class DLList {
 public:
-    // **Important**
-    // Normally you would make the Node struct PRIVATE
-    // but for this assignment it is important to keep it 
-    // PUBLIC
-    // Also, the declaration of Node needs to come 
-    // before everything else in the class, so leave it here
-    // at the beginning.
+    // Node structure
     struct Node {
         Node* prev;
         Node* next;
@@ -22,56 +16,167 @@ public:
     };
 
 private:
-    // Put your private fields/methods here
+    Node* head;
+    Node* tail;
+    int list_size;
 
 public:
-    DLList() {}
+    DLList() : head(nullptr), tail(nullptr), list_size(0) {}
 
-    ~DLList() {}
+    ~DLList() {
+        clear();
+    }
 
     const Node* get_head() const {
-        // implement get_head here
+        return head;
     }
 
     void push_front(T item) {
-        // implement push_front here
+        Node* new_node = new Node(item);
+        if (head == nullptr) {
+            head = tail = new_node;
+        } else {
+            new_node->next = head;
+            head->prev = new_node;
+            head = new_node;
+        }
+        ++list_size;
     }
+
     void push_back(T item) {
-        // implement push_back here
+        Node* new_node = new Node(item);
+        if (tail == nullptr) {
+            head = tail = new_node;
+        } else {
+            new_node->prev = tail;
+            tail->next = new_node;
+            tail = new_node;
+        }
+        ++list_size;
     }
+
     void insert(T item, int position) {
-        // implement insert here
+        if (position < 0 || position > list_size) {
+            throw std::out_of_range("Index out of range");
+        }
+
+        if (position == 0) {
+            push_front(item);
+        } else if (position == list_size) {
+            push_back(item);
+        } else {
+            Node* new_node = new Node(item);
+            Node* current = head;
+            for (int i = 0; i < position; ++i) {
+                current = current->next;
+            }
+            new_node->prev = current->prev;
+            new_node->next = current;
+            current->prev->next = new_node;
+            current->prev = new_node;
+            ++list_size;
+        }
     }
 
     void pop_front() {
-        // implement pop_front here
+        if (head == nullptr) {
+            throw std::length_error("List is empty");
+        }
+
+        Node* temp = head;
+        head = head->next;
+        if (head != nullptr) {
+            head->prev = nullptr;
+        } else {
+            tail = nullptr;
+        }
+        delete temp;
+        --list_size;
     }
+
     void pop_back() {
-        // implement pop_back here
+        if (tail == nullptr) {
+            throw std::length_error("List is empty");
+        }
+
+        Node* temp = tail;
+        tail = tail->prev;
+        if (tail != nullptr) {
+            tail->next = nullptr;
+        } else {
+            head = nullptr;
+        }
+        delete temp;
+        --list_size;
     }
+
     void remove(int position) {
-        // implement remove here
+        if (position < 0 || position >= list_size) {
+            throw std::out_of_range("Index out of range");
+        }
+
+        if (position == 0) {
+            pop_front();
+        } else if (position == list_size - 1) {
+            pop_back();
+        } else {
+            Node* current = head;
+            for (int i = 0; i < position; ++i) {
+                current = current->next;
+            }
+            current->prev->next = current->next;
+            if (current->next != nullptr) {
+                current->next->prev = current->prev;
+            }
+            delete current;
+            --list_size;
+        }
     }
 
     const T& front() const {
-        // implement front here
+        if (head == nullptr) {
+            throw std::length_error("List is empty");
+        }
+        return head->value;
     }
+
     const T& back() const {
-        // implement back here
+        if (tail == nullptr) {
+            throw std::length_error("List is empty");
+        }
+        return tail->value;
     }
+
     const T& at(int index) const {
-        // implement at here
+        if (index < 0 || index >= list_size) {
+            throw std::out_of_range("Index out of range");
+        }
+
+        Node* current = head;
+        for (int i = 0; i < index; ++i) {
+            current = current->next;
+        }
+        return current->value;
     }
 
     bool contains(const T& item) const {
-        // implement contains here
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->value == item) {
+                return true;
+            }
+            current = current->next;
+        }
+        return false;
     }
 
     int size() const {
-        // implement size here
+        return list_size;
     }
 
     void clear() {
-        // implement clear here
+        while (head != nullptr) {
+            pop_front();
+        }
     }
 };
