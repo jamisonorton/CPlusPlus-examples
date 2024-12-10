f47d52ab740bfd44fa92cb2c5b6564abe78d6828
Jamison Orton <jorton4@byu.edu>
2024-12-10 08:17:22 -0700

    added make file templates

--- Homework4a/SLList.h
+++ Homework4a/SLList.h
@@ -10,31 +10,73 @@ public:
         Node(T v) : next(nullptr), value(v) {}
     };
 
-    SLList() {}
+    // Default constructor that initializes an empty list
+    SLList() : head(nullptr), list_size(0) {}
 
-    ~SLList() {}
+    // Destructor that clears the list
+    ~SLList() {
+        clear();
+    }
 
+    // Returns a pointer to the head node
     const Node* get_head() const {
-        // implement get_head here
+        return head;
     }
 
+    // Adds a new node with the given item to the end of the list
     void push_back(T item) {
-        // implement push_back here
+        Node* new_node = new Node(item);
+        if (!head) {
+            head = new_node;
+        } else {
+            Node* current = head;
+            while (current->next) {
+                current = current->next;
+            }
+            current->next = new_node;
+        }
+        ++list_size;
     }
 
+    // Removes the last node in the list
     void pop_back() {
-        // implement pop_back here
+        if (head) {
+            if (!head->next) {
+                delete head;
+                head = nullptr;
+            } else {
+                Node* current = head;
+                while (current->next && current->next->next) {
+                    current = current->next;
+                }
+                delete current->next;
+                current->next = nullptr;
+            }
+            --list_size;
+        }
     }
 
+    // Returns a const reference to the value of the head node
     const T& front() const {
-        // implement front here
+        return head->value;
     }
 
+    // Returns the current size of the list
     int size() const {
-        // implement size here
+        return list_size;
     }
 
+    // Removes all nodes from the list
     void clear() {
-        // implement clear here
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+        list_size = 0;
     }
+
+private:
+    Node* head;
+    int list_size;
 };
