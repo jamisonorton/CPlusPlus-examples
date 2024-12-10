6b0e2764d20415ba84aa153804043e3c277dedc1
Jamison Orton <jorton4@byu.edu>
2024-12-10 12:36:11 -0700

    updated make files to work from start

--- Homework3d/QS.h
+++ Homework3d/QS.h
@@ -1,21 +1,76 @@
 #pragma once
 
 #include <vector>
+#include <algorithm>  // For std::swap
 
 template<class T>
 int medianOfThree(std::vector<T>& array, int left, int right) {
-    // implement medianOfThree here
+    int middle = (left + right) / 2;
+
+    // Sort the three elements at left, middle, and right
+    if (array[left] > array[middle]) {
+        std::swap(array[left], array[middle]);
+    }
+    if (array[left] > array[right]) {
+        std::swap(array[left], array[right]);
+    }
+    if (array[middle] > array[right]) {
+        std::swap(array[middle], array[right]);
+    }
+
+    // Return the index of the median element (which will be the middle after sorting)
+    return middle;
 }
 
 template<class T>
 int partition(std::vector<T>& array, int left, int right) {
-    // implement partition here
+    // Choose pivot using median of three
+    int middle = medianOfThree(array, left, right);
+    T pivot = array[middle];
+
+    // Move the pivot to the leftmost position
+    std::swap(array[left], array[middle]);
+
+    int up = left + 1;
+    int down = right;
+
+    // Perform partitioning
+    while (up <= down) {
+        // Increment 'up' while array[up] <= pivot
+        while (up <= right && array[up] <= pivot) {
+            up++;
+        }
+
+        // Decrement 'down' while array[down] > pivot
+        while (array[down] > pivot) {
+            down--;
+        }
+
+        // Swap elements if up is still to the left of down
+        if (up < down) {
+            std::swap(array[up], array[down]);
+        }
+    }
+
+    // Place the pivot in its correct position
+    std::swap(array[left], array[down]);
+
+    return down;  // Return pivot index
+}
+
+template<class T>
+void sort_helper(std::vector<T>& array, int left, int right) {
+    if (left < right) {
+        // Partition the array and get the pivot index
+        int pivot_index = partition(array, left, right);
+
+        // Recursively sort the left and right subarrays
+        sort_helper(array, left, pivot_index - 1);
+        sort_helper(array, pivot_index + 1, right);
+    }
 }
 
 template<class T>
 void sort(std::vector<T>& array) {
-    // implement sort here
-    // hint: you'll probably want to make a recursive sort_helper function
+    sort_helper(array, 0, array.size() - 1);
 }
-
-
--- Homework5c/rebalance.h
+++ Homework5c/rebalance.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+
 struct Node {
     Node* left;
     Node* right;
@@ -9,17 +11,77 @@ struct Node {
     Node(int v) : left(nullptr), right(nullptr), value(v), height(1) {}
 };
 
-// Hint: you might find it helpful to write an update_height function that takes
-// a Node* and updates its height field based on the heights of its children
+// Helper function to update the height of a node
+void update_height(Node* node) {
+    if (node) {
+        int left_height = node->left ? node->left->height : 0;
+        int right_height = node->right ? node->right->height : 0;
+        node->height = 1 + std::max(left_height, right_height);
+    }
+}
 
+// Perform a left rotation
 void promote_left(Node*& root) {
-    // implement promote_left here
+    Node* new_root = root->right;
+    root->right = new_root->left;
+    new_root->left = root;
+
+    // Update heights of the affected nodes
+    update_height(root);
+    update_height(new_root);
+
+    // Update root pointer
+    root = new_root;
 }
 
+// Perform a right rotation
 void promote_right(Node*& root) {
-    // implement promote_right here
+    Node* new_root = root->left;
+    root->left = new_root->right;
+    new_root->right = root;
+
+    // Update heights of the affected nodes
+    update_height(root);
+    update_height(new_root);
+
+    // Update root pointer
+    root = new_root;
 }
 
+// Calculate the balance factor of a node
+int get_balance_factor(Node* node) {
+    if (!node) return 0;
+    int left_height = node->left ? node->left->height : 0;
+    int right_height = node->right ? node->right->height : 0;
+    return left_height - right_height;
+}
+
+// Rebalance the tree at the given node
 void rebalance(Node*& root) {
-    // implement rebalance here
+    if (!root) return;
+
+    // Update the height of the current node
+    update_height(root);
+
+    // Get the balance factor
+    int balance_factor = get_balance_factor(root);
+
+    // Left-heavy subtree
+    if (balance_factor > 1) {
+        // Left-right case
+        if (get_balance_factor(root->left) < 0) {
+            promote_left(root->left);
+        }
+        // Left-left case
+        promote_right(root);
+    }
+    // Right-heavy subtree
+    else if (balance_factor < -1) {
+        // Right-left case
+        if (get_balance_factor(root->right) > 0) {
+            promote_right(root->right);
+        }
+        // Right-right case
+        promote_left(root);
+    }
 }
